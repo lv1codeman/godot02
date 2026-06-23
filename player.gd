d02d029dfed9c55dae74c8d8a26f39a3adb47db6
lv1codeman <lv1codeman@gmail.com>
2026-06-23 11:06:33 +0800

    修正landing bug，攻擊製作中

--- player.gd
+++ player.gd
@@ -6,14 +6,22 @@ enum State{
 	JUMP,
 	FALL,
 	LANDING,
+	ATTACK_1,
+	ATTACK_2,
 }
 
-const GROUND_STATES := [State.IDLE, State.RUNNING, State.LANDING]
+const GROUND_STATES := [
+	State.IDLE, State.RUNNING, State.LANDING, 
+	State.ATTACK_1, State.ATTACK_2
+	]
 const RUN_SPEED := 160.0
 const JUMP_VELOCITY := -300.0
 
+@export var can_combo := false
+
 var default_gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 var is_first_tick := false
+var is_combo_requested := false
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -28,7 +36,9 @@ func _unhandled_input(event: InputEvent) -> void:
 		jump_request_timer.stop()
 		if velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
-
+	
+	if event.is_action_pressed("attack") and can_combo:
+		is_combo_requested = true
 
 func tick_physics(state: State, delta: float) -> void:
 	match state:
@@ -41,8 +51,10 @@ func tick_physics(state: State, delta: float) -> void:
 		State.FALL:
 			move(default_gravity, delta)
 		State.LANDING:
-			move(default_gravity, delta)
-			
+			stand(delta)
+		State.ATTACK_1, State.ATTACK_2:
+			stand(delta)
+		
 	is_first_tick = false
 			
 			
@@ -56,6 +68,10 @@ func move(gravity: float, delta: float) -> void:
 		
 	move_and_slide()
 	
+func stand(delta: float) -> void:
+	velocity.x = 0.0
+	velocity.y += default_gravity * delta
+	move_and_slide()
 	
 func get_next_state(state: State) -> State:
 	var can_jump := is_on_floor() or coyote_timer.time_left > 0
@@ -65,18 +81,22 @@ func get_next_state(state: State) -> State:
 	if should_jump:
 		return State.JUMP
 	
+	# 在任何情況下，state為GROUND_STATES但又不在地板上時，轉換到FALL狀態
+	if state in GROUND_STATES and not is_on_floor():
+		return State.FALL
+	
 	var direction := Input.get_axis("move_left","move_right")
 	var is_still := is_zero_approx(direction) and is_zero_approx(velocity.x)
 		
 	match state:
 		State.IDLE:
-			if not is_on_floor():
-				return State.FALL
+			if Input.is_action_just_pressed("attack"):
+				return State.ATTACK_1
 			if not is_still:
 				return State.RUNNING
 		State.RUNNING:
-			if not is_on_floor():
-				return State.FALL
+			if Input.is_action_just_pressed("attack"):
+				return State.ATTACK_1
 			if is_still:
 				return State.IDLE
 		State.JUMP:
@@ -84,11 +104,18 @@ func get_next_state(state: State) -> State:
 				return State.FALL
 		State.FALL:
 			if is_on_floor():
-				return State.LANDING
+				return State.LANDING if is_still else State.RUNNING
 		State.LANDING:
+			if not is_still:
+				return State.RUNNING
 			if not animation_player.is_playing():
 				return State.IDLE
-		
+		State.ATTACK_1:
+			if not animation_player.is_playing():
+				return State.ATTACK_2 if is_combo_requested else State.IDLE
+		State.ATTACK_2:
+			if not animation_player.is_playing():
+				return State.IDLE
 	return state
 
 func transition_state(from: State, to: State) -> void:
@@ -111,4 +138,11 @@ func transition_state(from: State, to: State) -> void:
 				coyote_timer.start()
 		State.LANDING:
 			animation_player.play("landing")
+		State.ATTACK_1:
+			animation_player.play("attack_1")
+			is_combo_requested = false
+		State.ATTACK_2:
+			animation_player.play("attack_2")
+			is_combo_requested = false
+			
 	is_first_tick = true
