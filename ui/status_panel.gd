63580b44c784e042e57211dcc7e935cc0777c88e
lv1codeman <lv1codeman@gmail.com>
2026-06-26 14:28:30 +0800

    角色血條完成_補上紅底和扣血動畫

--- ui/status_panel.gd
+++ ui/status_panel.gd
@@ -3,6 +3,7 @@ extends HBoxContainer
 @export var stats: Stats
 
 @onready var health_bar: TextureProgressBar = $HealthBar
+@onready var eased_healthe_bar: TextureProgressBar = $HealthBar/EasedHealtheBar
 
 func _ready() -> void:
 	stats.health_changed.connect(update_health)
@@ -11,3 +12,5 @@ func _ready() -> void:
 func update_health() -> void:
 	var percentage := stats.health / float(stats.max_health)
 	health_bar.value = percentage
+	
+	create_tween().tween_property(eased_healthe_bar, "value", percentage, 0.3)
