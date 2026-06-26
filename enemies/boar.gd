4ebb61504523f70b95b12deb258a78cd543520ea
lv1codeman <lv1codeman@gmail.com>
2026-06-26 10:43:52 +0800

    野豬死亡效果，人物攻擊前搖縮短

--- enemies/boar.gd
+++ enemies/boar.gd
@@ -4,18 +4,25 @@ enum State {
 	IDLE,
 	WALK,
 	RUN,
+	HURT,
+	DYING,
 }
 
-
+var pending_damage: Damage
+const KNOCKBACK_AMOUNT := 512.0
 @onready var wall_checker: RayCast2D = $Graphics/WallChecker
 @onready var player_checker: RayCast2D = $Graphics/PlayerChecker
 @onready var floor_checker: RayCast2D = $Graphics/FloorChecker
 @onready var calm_down_timer: Timer = $CalmDownTimer
 
+func can_see_player() -> bool:
+	if not player_checker.is_colliding():
+		return false
+	return player_checker.get_collider() is Player
 
 func tick_physics(state: State, delta: float) -> void:
 	match state:
-		State.IDLE:
+		State.IDLE, State.HURT, State.DYING:
 			move(0.0, delta)
 		State.WALK:
 			move(max_speed / 3, delta)
@@ -23,26 +30,36 @@ func tick_physics(state: State, delta: float) -> void:
 			if wall_checker.is_colliding() or not floor_checker.is_colliding():
 				direction *= -1 # 野豬轉身
 			move(max_speed, delta)
-			if player_checker.is_colliding():
+			if can_see_player():
 				calm_down_timer.start()
 
 func get_next_state(state: State) -> State:
-	if player_checker.is_colliding():
-		return State.RUN
+	if stats.health == 0:
+		return State.DYING
+		
+	if pending_damage:
+		return State.HURT	
 		
 	match state:
 		State.IDLE:
+			if can_see_player():
+				return State.RUN
 			if state_machine.state_time > 2:
 				return State.WALK
 				
 		State.WALK:
+			if can_see_player():
+				return State.RUN
 			if wall_checker.is_colliding() or not floor_checker.is_colliding():
 				return State.IDLE
 				
 		State.RUN:
-			if calm_down_timer.is_stopped():
+			if not can_see_player() and calm_down_timer.is_stopped():
 				return State.WALK
-				
+		
+		State.HURT:
+			if not animation_player.is_playing():
+				return State.RUN
 	return state
 			
 func transition_state(from: State, to: State) -> void:
@@ -67,6 +84,35 @@ func transition_state(from: State, to: State) -> void:
 		State.RUN:
 			animation_player.play("run")
 
-
+		State.HURT:
+			animation_player.play("hit")
+			
+			stats.health -= pending_damage.amount
+			print("Boar health: %s" % stats.health)
+			var dir := pending_damage.source.global_position.direction_to(global_position)
+			velocity = dir * KNOCKBACK_AMOUNT
+			
+			if dir.x > 0:
+				direction = Direction.LEFT
+			else:
+				direction = Direction.RIGHT
+				
+			pending_damage = null
+			
+		State.DYING:
+			animation_player.play("die")
 func _on_hurtbox_hurt(hitbox: Hitbox) -> void:
-	print("Ouch!")
+	pending_damage = Damage.new()
+	pending_damage.amount = 1
+	pending_damage.source = hitbox.owner
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
